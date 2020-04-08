cfcf951156e4d1f994ebba141a17eee3a1dd6786
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-08 12:48:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,8 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
+  projects = {}
 
   binding.pry
 end
