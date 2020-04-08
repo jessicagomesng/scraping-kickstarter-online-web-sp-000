2887b39740dcebf4c7e7692d8f1a196fb72bb49e
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-08 12:24:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,4 @@ end
 create_project_hash
 
 #project = kickstarter.css("li.project.grid_4")
-#project_title = project.css("h2.bbcard_name strong.a").text
+#project_title = project.css("h2.bbcard_name strong a").text
