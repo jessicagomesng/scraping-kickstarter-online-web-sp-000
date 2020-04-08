fe309b3f14bbd3e2aece0a5696022b07e2e20fb6
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-08 12:51:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,15 +11,19 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta li a span.location_name").text,
+      
+
+    }
   end
   projects
 end
 
 create_project_hash
 
-#project = kickstarter.css("li.project.grid_4")
-#project_thumbnail = project.css("div.project-thumbnail a img").attribute("src").value
-#project_description = project.css("p.bbcard_blurb").text
+
 #project_location = project.css("ul.project-meta li a span.location_name").text
 #funding = project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
